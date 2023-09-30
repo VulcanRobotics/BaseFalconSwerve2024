bcb1e43eb3e97cb8e987732417a7c7ff14f8314e
Cameron Lyon <91966745+CameronLyon@users.noreply.github.com>
2023-09-30 14:45:15 -0400

    Comments!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,32 +38,19 @@ public class RobotContainer {
     private final int translationAxis = XboxController.Axis.kLeftY.value;
     private final int strafeAxis = XboxController.Axis.kLeftX.value;
     private final int rotationAxis = XboxController.Axis.kRightX.value + seekRotationAxis;
-    
-
-    //private final float spit = XboxController.Axis.kLeftTrigger.value;
-    //private final float intake = XboxController.Axis.kRightTrigger.value;
-    
-    /* Driver Buttons */
-    
+    /* Operator Buttons */
     private final JoystickButton robotCentric = new JoystickButton(driver.getHID(), XboxController.Button.kBack.value);
-    //private final JoystickButton calibrateEncoders = new JoystickButton(driver, XboxController.Button.kStart.value);
-    
-    //private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton highPlace = new JoystickButton(operator, 5);
     private final JoystickButton midPlace = new JoystickButton(operator, 4);
     private final JoystickButton originPlace = new JoystickButton(operator, 2);
     private final JoystickButton claw = new JoystickButton(operator, 1);
-    /*private final JoystickButton drop = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-    private final JoystickButton intake = new JoystickButton(driver, XboxController.Axis.kRightTrigger.value);
-    private final JoystickButton spit = new JoystickButton(driver, XboxController.Axis.kLeftTrigger.value);
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
     private final VisionSubsystem s_Vision = new VisionSubsystem(s_Swerve);
-    private final ArmSubsystem s_Arm = new ArmSubsystem(driver.getHID());
+    private final ArmSubsystem s_Arm = new ArmSubsystem();
     private final PneumaticSubsystem s_Pneumatic = new PneumaticSubsystem();
     private final IntakeSubsystem s_Intake = new IntakeSubsystem();
     private final LightingSubsystem s_Light = new LightingSubsystem();
-    //private final RobotContainer s_RobotContainer = new RobotContainer();
     public AutoManager autoManager = new AutoManager(s_Swerve, s_Arm, s_Pneumatic, s_Intake, s_Vision, s_Light);
 
     private Boolean isFieldRelative = true;
@@ -76,7 +63,7 @@ public class RobotContainer {
     public RobotContainer() {
         //System.out.println("Alliance: " + DriverStation.getAlliance() + "...................");
 
-        s_Swerve.setDefaultCommand(
+        s_Swerve.setDefaultCommand( //Unless overrided, this command takes in xbox inputs and controls the robot's swerve modules
             new TeleopSwerve(
                 s_Swerve, 
                 () -> -driver.getRawAxis(translationAxis), 
@@ -87,25 +74,15 @@ public class RobotContainer {
             )
         );
         
-        s_Arm.setDefaultCommand(
+        s_Arm.setDefaultCommand( //Unless overrided, this command takes in joystick inputs and controls the robot's arm
             new JoystickMovement(s_Arm, operator)
         );
 
         // Configure the button bindings
         configureButtonBindings();
 
-        //s_Arm.setDefaultCommand(new InstantCommand(() -> s_Arm.joystickMovement(operator.getX(), operator.getY(), operator.getZ()), s_Arm));
-    }
 
-    /*public void joystickToModuleState() {
-        new CubeSeek(
-            s_Swerve, 
-        () -> -0.2, 
-        () -> -0.0, 
-        () -> -0.0,
-        //() -> driver.back().getAsBoolean(),
-        () -> isFieldRelative);
-    }*/
+    }
 
 
     public Swerve getSwerve() {
@@ -114,6 +91,7 @@ public class RobotContainer {
         }
         else return null;
     }
+
     /**
      * Use this method to define your button->command mappings. Buttons can be created by
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -122,28 +100,6 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         /* Driver Buttons */
-
-        // Switch between field-relative and robot-centric driving
-        robotCentric.onTrue(new InstantCommand(() -> {
-          isFieldRelative = !isFieldRelative;
-        }));
-
-        // zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
-        // calibrateEncoders.onTrue(new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()));
-        highPlace.whileTrue(new HighPlace(s_Arm));
-        //highPlace.onFalse(new JoystickMovement(s_Arm, operator));
-        midPlace.whileTrue(new MidPlace(s_Arm));
-        //midPlace.onFalse(new JoystickMovement(s_Arm, operator));
-        originPlace.whileTrue(new OriginPlace(s_Arm));
-        //originPlace.onFalse(new JoystickMovement(s_Arm, operator));
-        claw.onTrue(new InstantCommand(() -> s_Pneumatic.toggleClawState()));
-        /*drop.onTrue(new InstantCommand(() -> s_Pneumatic.setIntakeState(true)));
-        drop.onFalse(new InstantCommand(() -> s_Pneumatic.setIntakeState(false)));
-        intake.whileTrue(new InstantCommand(() -> s_Intake.intake()));
-        intake.whileFalse(new InstantCommand(() -> s_Intake.holdBall()));
-        spit.whileTrue(new InstantCommand(() -> s_Intake.spit()));*/
-
-        
         driver.leftTrigger(0.5).whileTrue(new InstantCommand(() -> s_Intake.forceSpit()));
         driver.leftBumper().onTrue(new InstantCommand(() -> s_Vision.findIt()));
         driver.rightTrigger(0.5).onTrue(new InstantCommand(() -> s_Intake.intake()));
@@ -152,20 +108,19 @@ public class RobotContainer {
         driver.rightTrigger(0.5).onFalse(new InstantCommand(() -> s_Pneumatic.setIntakeState(false)));
         driver.y().onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
         driver.start().onTrue(new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()));
-        
-        
-        
-        /*if (spit > 0.5){
-            new InstantCommand(() -> s_Intake.spit());
-        }
-        
-        if (intake > 0.5){
-            new InstantCommand(() -> s_Intake.intake());
-            new InstantCommand(() -> s_Pneumatic.setIntakeState(true));
-        } else if (spit < 0.5) {
-            new InstantCommand(() -> s_Intake.holdBall());
-            new InstantCommand(() -> s_Pneumatic.setIntakeState(false));
-        }*/
+
+        /* Operator Buttons */
+        highPlace.whileTrue(new HighPlace(s_Arm));
+        midPlace.whileTrue(new MidPlace(s_Arm));
+        originPlace.whileTrue(new OriginPlace(s_Arm));
+        claw.onTrue(new InstantCommand(() -> s_Pneumatic.toggleClawState()));
+
+
+        // Switch between field-relative and robot-centric driving
+        robotCentric.onTrue(new InstantCommand(() -> {
+            isFieldRelative = !isFieldRelative;
+            }));
+
         
 
     }
